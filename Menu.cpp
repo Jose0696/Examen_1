e5d047332410fdcf39c8f8c566e159dad7a86375
Jose Miguel <53483639+Jose0696@users.noreply.github.com>
2022-09-15 10:08:01 -0600

    Menu

---
+++ Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jose on 15/9/2022.
+//
+
+#include "Menu.h"
---
+++ Menu.h
@@ -0,0 +1,17 @@
+//
+// Created by Jose on 15/9/2022.
+//
+
+#ifndef EXAMEN_1_MENU_H
+#define EXAMEN_1_MENU_H
+#include "Utils.h"
+
+class Menu {
+private:
+
+public:
+
+};
+
+
+#endif //EXAMEN_1_MENU_H
